429344153707e967a371f399a9a052d4e58ecc55
David Hundley <david.k.hundley@gmail.com>
2020-03-01 09:20:20 -0600

    Updates to config file

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 #----------------------------------------------------------------------------#
 # Imports
 #----------------------------------------------------------------------------#
-
 import json
 import dateutil.parser
 import babel
@@ -512,11 +511,4 @@ if not app.debug:
 
 # Default port:
 if __name__ == '__main__':
-    app.run()
-
-# Or specify port manually:
-'''
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-'''
+    app.run(debug = True)
